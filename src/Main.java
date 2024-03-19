52f5cc1dd4ba7765e7dca74eeb9eebd4e0747787
tejeswarrajuvempalli <vtejas099@gmail.com>
2024-03-19 14:38:07 -0500

    checking is this working.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Tejeswar. This is your first code to push into GitHub. All the Beat");
+		System.out.println("Checking is this working");
 	}
 }
